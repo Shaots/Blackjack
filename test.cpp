0e8831b38b9eb06f05aa3e7ba84d1494587f2c87
Shao tsziatsi <shaots@mail.ru>
2024-06-04 17:13:33 +0300

    Add test and test it.

--- test.cpp
+++ test.cpp
@@ -1,11 +1,22 @@
 
 #include "test.h"
 
-void test1(){
+void test1() {
     srand(static_cast<unsigned int>(time(0)));
     std::array<Card, MAX_SUITS * MAX_RANK> deck = createDeck();
     std::cout << toStringDeck(deck) << std::endl;
     shuffleDeck(deck);
     std::cout << toStringDeck(deck) << std::endl;
     std::cout << getCardValue(deck[0]);
+}
+
+void test2() {
+    srand(static_cast<unsigned int>(time(0)));
+    std::array<Card, MAX_SUITS * MAX_RANK> deck = createDeck();
+    shuffleDeck(deck);
+    std::cout << toStringDeck(deck) << std::endl;
+    if (playBlackJack(deck))
+        std::cout << "You win!\n";
+    else
+        std::cout << "You lose!\n";
 }
--- test.h
+++ test.h
@@ -3,9 +3,11 @@
 #define BLACKJACK_TEST_H
 
 #include <iostream>
+#include "BlackJack.h"
 #include "Card.h"
 
 void test1();
 
+void test2();
 
 #endif //BLACKJACK_TEST_H
